3b7c1da0b1c0fc8ae4705734cc7c24c9c22c8c3a
psrth <parthsharma151@gmail.com>
2022-12-07 16:19:34 +0530

    [add]: integration with GOORM API

--- erickshaw-client/src/App.js
+++ erickshaw-client/src/App.js
@@ -1,11 +1,62 @@
-import React, { useState } from "react";
-import { Flex, Text, Image } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Flex, Text, Image, useToast } from "@chakra-ui/react";
 import Chart from "./Chart";
 import auto from "./auto.png";
 
 function App() {
   const [speed, setSpeed] = useState(["24.4", "22", "21"]);
   const [speedLabels, setSpeedLabels] = useState(["1", "2", "3"]);
+  const [crash, setCrash] = useState(0);
+  const [gryo, setGryo] = useState("25.54");
+
+  const toast = useToast();
+
+  const pushSpeedData = (spt) => {
+    let temp = speed;
+    temp.push(spt);
+    setSpeed(temp);
+  };
+
+  const handleCrash = (crt) => {
+    if (crt > 20) setCrash(1);
+    else setCrash(0);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://parth-iot-intro.run-ap-south1.goorm.io/aws_iot_get/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) =>
+        response
+          .json()
+          .then((data) => ({
+            data: data,
+            status: response.status,
+          }))
+          .then((res) => {
+            if (res.status < 300) {
+              pushSpeedData(res.data.speed);
+              handleCrash(res.data.pressure);
+              setGryo(res.data.gryo);
+            } else {
+              toast({
+                title: "API Unresponsive.",
+                position: "top",
+                description:
+                  "An error occurred while retrieving data from API.",
+                status: "error",
+                duration: 2000,
+                isClosable: true,
+              });
+            }
+          })
+      );
+    }, 5000);
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <Flex
@@ -73,8 +124,13 @@ function App() {
           <Text color="#A3A3A3" fontWeight="600">
             CRASH DETECTION
           </Text>
-          <Text color="#00DF67" fontWeight="700" fontSize="24px" mt="2px">
-            No crashes detected.
+          <Text
+            color={crash ? "#FF4930" : "#00DF67"}
+            fontWeight="700"
+            fontSize="24px"
+            mt="2px"
+          >
+            {crash ? "CRASH DETECTED!" : "No crashes detected."}
           </Text>
         </Flex>
 
@@ -92,7 +148,7 @@ function App() {
             GYROSCOPE READING
           </Text>
           <Text color="#00DF67" fontWeight="700" fontSize="24px" mt="2px">
-            24.54
+            {gryo}
           </Text>
         </Flex>
 
